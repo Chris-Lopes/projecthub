0dabb5eeb6a13223576000cbeb38e35cd6fcaf64
Chris-Lopes <chrislopes3667@gmail.com>
2025-04-26 12:05:30 +0530

    working

--- src/app/api/socket/route.ts
+++ src/app/api/socket/route.ts
@@ -1,9 +1,9 @@
-import { initSocket, NextApiResponseServerIO } from "@/lib/socket";
+import { initSocketServer, NextApiResponseServerIO } from "@/lib/socket-server";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, res: NextApiResponseServerIO) {
   try {
-    const io = initSocket(res);
+    const io = initSocketServer(res);
     return new Response("Socket initialized", { status: 200 });
   } catch (error) {
     console.error("Socket initialization error:", error);
